69a23e98065ef00f24f31d3ba006397d3eb39363
Vivek Chhabra <vivekchhabra@Viveks-MacBook-Air.local>
2024-02-06 22:48:07 +0530

    spring boot security test

---
+++ src/main/java/com/vivek/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.vivek.security.configuration;
+
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfiguration extends WebSecurityConfiguration {
+
+}
