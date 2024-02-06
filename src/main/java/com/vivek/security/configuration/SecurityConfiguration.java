c8b0beb4a5dde055d28d28737a61d1c84d19c70a
VivekChhabra <vchhabra949@gmail.com>
2024-02-06 23:01:23 +0530

    Security Changes

--- src/main/java/com/vivek/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/vivek/security/configuration/SecurityConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
-public class SecurityConfiguration extends WebSecurityConfiguration {
+public class  SecurityConfiguration extends WebSecurityConfiguration {
 
 }
